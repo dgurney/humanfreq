ebe1b42528f409bbab8370df852e42c7b56646f2
Daniel Gurney <dgurney99@gmail.com>
2019-08-24 12:41:43 +0300

    Quick and dirty MHz scaling_cur_freq reader

---
+++ cpufreq_linux.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func readCPUFreq(cpu int) (int, error) {
+	path := "/sys/devices/system/cpu/cpu" + strconv.Itoa(cpu) + "/cpufreq/scaling_cur_freq"
+	freq, err := ioutil.ReadFile(path)
+	if err != nil {
+		return 0, err
+	}
+	final, err := strconv.Atoi(strings.TrimSuffix(string(freq), "\n"))
+	if err != nil {
+		return 0, err
+	}
+	return final, nil
+}
+
+func main() {
+	for {
+		c := exec.Command("clear")
+		c.Stdout = os.Stdout
+		c.Run()
+		for i := 0; i < runtime.NumCPU(); i++ {
+			freq, err := readCPUFreq(i)
+			if err != nil {
+				fmt.Println("CPU frequency read error:", err)
+				return
+			}
+			mhz := freq / 1000
+			fmt.Printf("CPU%d: %d MHz\n", i, mhz)
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+}
